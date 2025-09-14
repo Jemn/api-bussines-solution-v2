a02ac62fc6b9b7976318277562f9db5d16b5f1c1
Jemn <juliomateo@outlook.com>
2025-09-14 16:46:19 -0500

    quito bean

--- src/main/java/com/solution/mateo/ApiBussinesSolutionApplication.java
+++ src/main/java/com/solution/mateo/ApiBussinesSolutionApplication.java
@@ -2,8 +2,10 @@ package com.solution.mateo;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 
@@ -13,22 +15,22 @@ public class ApiBussinesSolutionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiBussinesSolutionApplication.class, args);
-
-		/** Implementación de contrapresión **/
-
-		Flux.range(1, 100)
-				.onBackpressureBuffer(10) // Buffer size to handle overflow
-				.publishOn(Schedulers.parallel())
-				.subscribe(data -> {
-					try {
-						Thread.sleep(50); // Simulate slow consumer
-						System.out.println("Consumed: " + data);
-					} catch (InterruptedException e) {
-						Thread.currentThread().interrupt();
-					}
-				});
-
-
 	}
 
+	/*@Bean
+	CommandLineRunner demoFlux() {
+		return args -> {
+			Flux.range(1, 100)
+					.onBackpressureBuffer(10) // Buffer limitado
+					.publishOn(Schedulers.parallel())
+					.subscribe(data -> {
+						try {
+							Thread.sleep(50); // Consumidor lento
+							System.out.println("Consumed: " + data);
+						} catch (InterruptedException e) {
+							Thread.currentThread().interrupt();
+						}
+					});
+		};
+	}*/
 }
